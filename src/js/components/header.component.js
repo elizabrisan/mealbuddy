6ddbbb696914e7e5b024a3ee7a2a713d5c2bce5f
Cornel <cornel.stefanache@gmail.com>
2018-02-03 16:36:11 +0200

    added icons

--- src/js/components/header.component.js
+++ src/js/components/header.component.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import SearchBar from './searchbar';
+import { Link } from 'react-router-dom'
 
 class Header extends React.Component {
 
   render() {
     return (<nav className="navbar ">
-      <SearchBar />
-      <span className="button float-right">test</span>
+      <div style={{
+          marginRight: 100,
+          width: '100%'
+        }}>
+        <SearchBar/>
+      </div>
+      <div style={{position: 'absolute', right: 10}}>
+        <Link to="/fridge"><i className="fa fa-list-alt fa-2x" title="list"></i></Link>
+        <Link to="/fridge"><i className="fa fa-archive fa-2x" title="fridge"></i></Link>
+      </div>
     </nav>)
   }
 }
